77a4a066eef1e5d391781a7cac9d4a1e08cf0885
Anatolii Iatsuk <35109939+tolik0@users.noreply.github.com>
2019-09-19 15:57:14 +0300

    Update compute_new_temperature.py

--- compute_new_temperature.py
+++ compute_new_temperature.py
@@ -10,7 +10,7 @@ def read_config(path):
     with open(path, "r") as file:
         config_args = dict([line.split("=") for line in file if not line.strip().startswith("#")])
 
-    arguments = ["dt", "dx", "dy", "k", "c", "density"])
+    arguments = ["dt", "dx", "dy", "k", "c", "density"]
     dt, dx, dy, k, c, density = map(lambda x: float(config_args[x]), arguments)
 
     with open(config_args["path_to_matrix"], "r") as file:
