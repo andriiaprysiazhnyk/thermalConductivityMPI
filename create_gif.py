c27124713a2f44d9638aa98b0036a94a28477d95
Anatolii Iatsuk <35109939+tolik0@users.noreply.github.com>
2019-09-24 21:11:44 +0300

    Add script for creating gif

---
+++ create_gif.py
@@ -0,0 +1,27 @@
+from absl import flags, app
+import imageio
+from os import listdir
+from os.path import join
+
+FLAGS = flags.FLAGS
+
+flags.DEFINE_string("images_path", "images/", "Path to configuration file.")
+flags.DEFINE_string("output_path", "heatmap.gif", "Path to configuration file.")
+flags.DEFINE_float("time", 0.5, "Time between frames.")
+
+
+def create_gif(images_path, output_path, time):
+    filenames = [join(images_path, f) for f in listdir(images_path) if f.endswith(".bmp")]
+    with imageio.get_writer(output_path, mode='I', duration=time) as writer:
+        for filename in filenames:
+            image = imageio.imread(filename)
+            writer.append_data(image)
+
+
+def main(argv):
+    del argv  # Unused.
+    create_gif(FLAGS.images_path, FLAGS.output_path, FLAGS.time)
+
+
+if __name__ == "__main__":
+    app.run(main)
