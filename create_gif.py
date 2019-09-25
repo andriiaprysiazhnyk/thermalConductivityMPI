13c63060985bff6bc05cb26cba02a7d657cd435f
Anatolii Iatsuk <35109939+tolik0@users.noreply.github.com>
2019-09-25 11:23:47 +0300

    Add sorting images

--- create_gif.py
+++ create_gif.py
@@ -5,15 +5,15 @@ from os.path import join
 
 FLAGS = flags.FLAGS
 
-flags.DEFINE_string("images_path", "images/", "Path to configuration file.")
+flags.DEFINE_string("images_path", "images/", "Path to folder file.")
 flags.DEFINE_string("output_path", "heatmap.gif", "Path to configuration file.")
-flags.DEFINE_float("time", 0.5, "Time between frames.")
+flags.DEFINE_float("time", 0.1, "Time between frames.")
 
 
 def create_gif(images_path, output_path, time):
     filenames = [join(images_path, f) for f in listdir(images_path) if f.endswith(".bmp")]
     with imageio.get_writer(output_path, mode='I', duration=time) as writer:
-        for filename in filenames:
+        for filename in sorted(filenames, key=lambda x: int(x.split("-")[1].split(".")[0])):
             image = imageio.imread(filename)
             writer.append_data(image)
 
